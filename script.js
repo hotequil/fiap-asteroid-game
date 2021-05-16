0efcd0ccf1e9557149a7b8bd54d480fda4ac350f
hotequil <joaohotequil@gmail.com>
2021-05-16 17:43:59 -0300

    feita parte de aumentar a dificuldade do game e do asteroíde aparecer

--- script.js
+++ script.js
@@ -7,23 +7,30 @@ if(!canvas?.getContext){
     throw Error(msg);
 }
 
-const sizeObj = 100;
+const sizeObj = 50;
 const initImg = () => new Image(sizeObj, sizeObj);
 const planeUp = initImg();
 const planeLeft = initImg();
 const planeRight = initImg();
 const planeDown = initImg();
-const asteroid = initImg();
+const asteroidUp = initImg();
+const asteroidLeft = initImg();
+const asteroidRight = initImg();
+const asteroidDown = initImg();
+let asteroid = initImg();
 let plane = initImg();
 
 planeUp.src = './imgs/plane-up.png';
 planeLeft.src = './imgs/plane-left.png';
 planeRight.src = './imgs/plane-right.png';
 planeDown.src = './imgs/plane-down.png';
-asteroid.src = './imgs/asteroid.png';
+asteroidUp.src = './imgs/asteroid-up.png';
+asteroidLeft.src = './imgs/asteroid-left.png';
+asteroidRight.src = './imgs/asteroid-right.png';
+asteroidDown.src = './imgs/asteroid-down.png';
 plane = planeUp;
 
-const initPlaneObj = () => ({x: (canvasWidth / 2) - (sizeObj / 2), y: (canvasHeight / 2) - (sizeObj / 2)});
+const initObj = isPlane => isPlane ? ({ x: (canvasWidth / 2) - (sizeObj / 2), y: (canvasHeight / 2) - (sizeObj / 2) }) : { x: -9999, y: -9999 };
 const btn = document.querySelector('[data-btn]');
 const canvasWidth = 800;
 const canvasHeight = 400;
@@ -31,16 +38,23 @@ const fontFam = 'Arial';
 const principalColor = '#000';
 const yellow = '#ffb91a';
 const red = '#ff1a1a';
-const warningColors = [yellow, '#ff841a', red];
+const orange = '#ff841a';
+const warningColors = [yellow, orange, red];
 const startColors = ['#1bf931', yellow, red];
 const ctx = canvas.getContext('2d');
 const defaultPoint = 10;
 const upKeyCode = 38;
 const oneSecond = 1000;
+const oneHundredMs = 100;
 const ftSzPoints = 20;
 const damagePoints = defaultPoint * 30;
-
-let planeObj = initPlaneObj();
+const validKeyCodes = [upKeyCode, 40, 39, 37];
+const asteroidsImgs = [asteroidUp, asteroidLeft, asteroidRight, asteroidDown];
+const topTxtHeight = canvasHeight - ftSzPoints;
+const lvls = ['fácil', 'médio', 'difícil'];
+const setLvl = lvlIdx => ({ txt: lvls[lvlIdx], color: warningColors[lvlIdx] })
+let planeObj = initObj(true);
+let asteroidObj = initObj();
 let intervalStart = null;
 let animationFrameIds = [];
 let intervalSec = null;
@@ -50,18 +64,68 @@ let timeToStart = null;
 let gameOver = null;
 let remainingSeconds = null;
 let isDamaged = null;
+let asteroidTimeout = null;
+let timesWithinAsteroidTimeout = 0;
+let changeAsteroidPos= false;
+let lvl = setLvl(0);
 
+const setRandomVal = (max = 0, min = 0) => Math.floor((Math.random() * (max - min) + min))
 const clear = () => ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-const drawPlane = img => ctx.drawImage(img, planeObj.x, planeObj.y, sizeObj, sizeObj);
-const resetRemainingSeconds = () => remainingSeconds = 10;
-const watchKeyDown = () => window.onkeydown = evt => paint(evt.keyCode);
+const resetRemainingSeconds = () => remainingSeconds = 5;
+const canChangeAsteroidPos = (time, rest, idxLvl) => timesWithinAsteroidTimeout >= time && timesWithinAsteroidTimeout % rest === 0 && lvls.indexOf(lvl.txt) <= idxLvl;
+
+const watchKeyDown = () => window.onkeydown = evt => {
+    const { keyCode } = evt;
+
+    validKeyCodes.includes(keyCode) ? paint(evt.keyCode) : null;
+};
+
+const watchAsteroidShow = () => {
+    asteroidTimeout = setInterval(() => {
+        if(timesWithinAsteroidTimeout !== 0){
+            if(canChangeAsteroidPos(100, 2, 2)){
+                lvl = setLvl(2);
+                changeAsteroidPos = true;
+            } else if(canChangeAsteroidPos(50, 5, 1)){
+                lvl = setLvl(1);
+                changeAsteroidPos = true;
+            } else if(canChangeAsteroidPos(0, 10, 0)) {
+                lvl = setLvl(0);
+                changeAsteroidPos = true;
+            }
+        }
+
+        ++timesWithinAsteroidTimeout;
+    }, oneHundredMs);
+}
+
+const selectAsteroid = () => {
+    if(changeAsteroidPos){
+        changeAsteroidPos = false;
+
+        const imgCurIdx = asteroidsImgs.indexOf(asteroid);
+
+        asteroid = imgCurIdx >= (asteroidsImgs.length - 1) ? asteroidsImgs[0] : asteroidsImgs[imgCurIdx + 1];
+        asteroidObj.x = setRandomVal(canvasWidth - sizeObj, 0)
+        asteroidObj.y = setRandomVal(canvasHeight - sizeObj, 0)
+    }
+}
+
+const draw = imgPlane => {
+    ctx.drawImage(imgPlane, planeObj.x, planeObj.y, sizeObj, sizeObj);
+    ctx.drawImage(asteroid, asteroidObj.x, asteroidObj.y, sizeObj, sizeObj);
+};
 
 const endGame = () => {
     gameOver = true;
-    planeObj = initPlaneObj();
+    planeObj = initObj(true);
+    asteroidObj = initObj();
+    lvl = setLvl(0);
+    timesWithinAsteroidTimeout = 0;
     window.onkeydown = null;
     animationFrameIds.forEach(id => window.cancelAnimationFrame(id));
     clearInterval(intervalStart);
+    clearInterval(asteroidTimeout);
     clearInterval(intervalSec);
 };
 
@@ -124,15 +188,18 @@ const paint = (keyCode, speed = 25) => {
     ctx.fillStyle = isDamaged ? red : principalColor;
     ctx.textBaseline = 'bottom';
     ctx.textAlign = 'left';
-    ctx.fillText(`Sua pontuação é: ${points}`, ftSzPoints, canvasHeight - ftSzPoints);
+    ctx.fillText(`Sua pontuação é: ${points}`, ftSzPoints, topTxtHeight);
+    ctx.fillStyle = lvl.color;
+    ctx.fillText(`Dificuldade do jogo: ${lvl.txt}`, ftSzPoints, topTxtHeight - ftSzPoints - 8);
 
     ctx.textBaseline = 'top';
     ctx.fillStyle = remainingSeconds >= 4 ? principalColor : [...warningColors].reverse()[remainingSeconds - 1];
     ctx.fillText(`Você pederá ${damagePoints} pontos se ficar parado por mais ${remainingSeconds} ${remainingSeconds > 1 ? 'segundos' : 'segundo'}`, ftSzPoints, ftSzPoints);
 
-    drawPlane(plane);
+    selectAsteroid();
+    draw(plane);
 
-    animationFrameIds.push(requestAnimationFrame(paint));
+    if(!gameOver) animationFrameIds.push(requestAnimationFrame(paint.bind(this, 0)));
 }
 
 const start = () => {
@@ -158,6 +225,7 @@ const start = () => {
             paint(upKeyCode, 0);
             watchKeyDown();
             checkSecondsEvts();
+            watchAsteroidShow();
 
             return;
         }
